14ce240e871e8d15d0909a97266d7136df37364a
KC Wesley <KCWx9@tuta.io>
2018-07-09 17:35:16 +0100

    remove ms-core

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import adminRoutes from '@/modules/admin/_routes/admin.routes.js'
-import focusRoutes from '@/modules/focus/_routes/focus.routes.js'
-import libraryRoutes from '@/modules/lib/_routes/library.routes.js'
 
 Vue.use(Router)
 
 const baseRoutes = []
 
 const routes = baseRoutes.concat(
-  adminRoutes,
-  focusRoutes,
-  libraryRoutes
+  adminRoutes
 )
 
 export default new Router({
