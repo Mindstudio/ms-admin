344dc8574cd19b245d2b91e77ea6c27d48731b92
KC Wesley <KCWx9@tuta.io>
2018-07-17 23:11:26 +0100

    vuex store update

--- src/store/index.js
+++ src/store/index.js
@@ -30,18 +30,6 @@ export default new Vuex.Store({
       }, (err) => {
         console.log(err)
       })
-    },
-    create_book: function () {
-      console.log('create_book action')
-      adminAPI.createBook({
-        title: this.title,
-        author: this.author,
-        summary: this.summary,
-        isbn: this.isbn,
-        genre: this.genre
-      }, (err) => {
-        console.log(err)
-      })
     }
   },
 
@@ -52,16 +40,6 @@ export default new Vuex.Store({
     set_books: (state, { payload }) => {
       state.books = payload
     }
-    // set_book_info: (state) => {
-    //   payload = {
-    //     title: this.title,
-    //     author: this.author,
-    //     summary: this.summary,
-    //     isbn: this.isbn,
-    //     genre: this.genre
-    //   }
-    //   console.log('set_book_info')
-    // }
   },
 
   modules: {}
