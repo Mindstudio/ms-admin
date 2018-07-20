2f149f3c67756aa0058c72749c6a972b7fb21f15
KC Wesley <KCWx9@tuta.io>
2018-07-20 15:16:56 +0100

    created viewDirItem, updateDirItem routes

--- src/modules/dir/_routes/dir.routes.js
+++ src/modules/dir/_routes/dir.routes.js
@@ -3,6 +3,8 @@ import adminMain from '@/views/adminMain'
 import dirMain from '../_containers/dirMain'
 
 import viewDir from '../_views/viewDir'
+import viewDirItem from '../_views/viewDirItem'
+import updateDirItem from '../_views/updateDirItem'
 
 export default [
   {
@@ -21,6 +23,16 @@ export default [
                 name: 'view-dir',
                 path: 'dir',
                 component: viewDir
+              },
+              {
+                name: 'view-dir-item',
+                path: 'view/:id',
+                component: viewDirItem
+              },
+              {
+                name: 'update-dir-item',
+                path: 'update/:id',
+                component: updateDirItem
               }
             ]
           }
