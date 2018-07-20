3b413267e9617fd56253acabf2230c63c5463b8f
KC Wesley <KCWx9@tuta.io>
2018-07-20 20:30:39 +0100

    update admin store

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import booksAPI from '@/modules/lib/_api/booksAPI'
 import authorsAPI from '@/modules/dir/_api/authorsAPI'
+
+import libModule from '@/modules/lib/_store/index'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    books: {},
-    authors: {}
+    authors: {},
+    user: {
+      name: 'Wesley'
+    }
   },
 
   getters: {
-    getBookById: (state) => (id) => {
-      return state.books.find(book => book._id === id)
-    },
     getAuthorById: (state) => (id) => {
       return state.authors.find(author => author._id === id)
     }
@@ -29,13 +29,6 @@ export default new Vuex.Store({
     //     console.log(err)
     //   })
     // },
-    load_books: function ({ commit }) {
-      booksAPI.findBooks().then((res) => {
-        commit('set_books', { payload: res.data })
-      }, (err) => {
-        console.log(err)
-      })
-    },
     load_authors: function ({ commit }) {
       authorsAPI.findAuthors().then((res) => {
         commit('set_authors', { payload: res.data })
@@ -49,14 +42,13 @@ export default new Vuex.Store({
     // set_items: (state, { payload }) => {
     //   state.data.items = payload
     // },
-    set_books: (state, { payload }) => {
-      state.books = payload
-    },
     set_authors: (state, { payload }) => {
       state.authors = payload
     }
   },
 
-  modules: {}
+  modules: {
+    libModule
+  }
 
 })
