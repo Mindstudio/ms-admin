8221c8d0286ae6dc3b29aa153323e1bf9e36fed3
KC Wesley <KCWx9@tuta.io>
2018-07-20 13:15:50 +0100

    change router setup

---
+++ src/router/admin.routes.js
@@ -0,0 +1,30 @@
+import adminContainer from '@/containers/Admin'
+import adminMain from '../views/adminMain'
+
+import createView from '../views/createView'
+import dash from '../views/dash'
+
+export default [
+  {
+    path: '/',
+    component: adminContainer,
+    children: [
+      {
+        path: '/',
+        component: adminMain,
+        children: [
+          {
+            name: 'create',
+            path: '/create',
+            component: createView
+          },
+          {
+            name: 'dash',
+            path: '/dash',
+            component: dash
+          }
+        ]
+      }
+    ]
+  }
+]
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import adminRoutes from '@/modules/admin/_routes/admin.routes.js'
+import adminRoutes from '@/router/admin.routes.js'
+import libRoutes from '@/modules/lib/_routes/lib.routes.js'
 
 Vue.use(Router)
 
 const baseRoutes = []
 
 const routes = baseRoutes.concat(
-  adminRoutes
+  adminRoutes,
+  libRoutes
 )
 
 export default new Router({
